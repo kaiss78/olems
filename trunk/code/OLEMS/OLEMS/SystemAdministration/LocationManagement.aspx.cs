f7266737247d53548414e0c5f3a026608c4fc210
erdem.hale <erdem.hale@7d986af4-2e36-11de-ae9b-2be1a451ffb1>
2009-05-31 17:56:59 +0000

    duzenlemeler

--- trunk/code/OLEMS/OLEMS/SystemAdministration/LocationManagement.aspx.cs
+++ trunk/code/OLEMS/OLEMS/SystemAdministration/LocationManagement.aspx.cs
@@ -11,48 +11,51 @@ using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
 using System.Xml.Linq;
 
-public partial class UI_QuestionDevelopment_LocationManagement : System.Web.UI.Page
+namespace OLEMS.SystemAdministration
 {
-    protected void Page_Load(object sender, EventArgs e)
+    public partial class LocationManagement : System.Web.UI.Page
     {
-
-    }
-
-    protected void lnkSearch_Click(object sender, EventArgs e)
-    {
-        String sorgu;
-        sorgu = "1=1";
-        LocationGridView.EditIndex = -1;
-        TextBox txtLocation = (TextBox) LocationDetailsView.Rows[0].FindControl("txtName");
-        sorgu = sorgu + " AND name like '%" + txtLocation.Text + "%'";
-        TextBox txtCapacity = (TextBox) LocationDetailsView.Rows[0].FindControl("txtCapacity");
-        if (txtCapacity.Text != "") //boş değilse
+        protected void Page_Load(object sender, EventArgs e)
         {
-            sorgu = sorgu + " AND capacity =" + Convert.ToInt16(txtCapacity.Text);
-        }
-        if (SearchAllCheckBox.Checked == false) //eğer tek bir bina seçildiyse
-        {
-            DropDownList building = (DropDownList) LocationDetailsView.Rows[0].FindControl("buildingDropDownList");
-            sorgu = sorgu + " AND locationBuildingId ='" + building.SelectedValue + "'";
-        }
-        Session["LocationFilter"] = sorgu; //filter bilgisini başka eventlerde kullanmak için session'da bir değişken olarak saklarız
-        Location_SqlDataSource.FilterExpression = sorgu;
-    }
-   
-    protected void ApplyFilter()
-    {
-       //bu yordam filtrelenmiş bir satırın edit edilebilmesi ve herhangi bir işlem yapıldıktan sonra filtrenin hala kalması için yazıldı
-        Location_SqlDataSource.FilterExpression = (string)(Session["LocationFilter"]);
-        LocationGridView.DataBind();
-    }
 
-    protected void LocationGridView_RowEditing(object sender, GridViewEditEventArgs e)
-    {
-        ApplyFilter();
-    }
-    
-    protected void LocationGridView_RowUpdated(object sender, GridViewUpdatedEventArgs e)
-    {
-        ApplyFilter();
+        }
+        protected void lnkSearch_Click(object sender, EventArgs e)
+        {
+            String sorgu;
+            sorgu = "1=1";
+            LocationGridView.EditIndex = -1;
+            TextBox txtLocation = (TextBox)LocationDetailsView.Rows[0].FindControl("txtName");
+            sorgu = sorgu + " AND name like '%" + txtLocation.Text + "%'";
+            TextBox txtCapacity = (TextBox)LocationDetailsView.Rows[0].FindControl("txtCapacity");
+            if (txtCapacity.Text != "") //boş değilse
+            {
+                sorgu = sorgu + " AND capacity =" + Convert.ToInt16(txtCapacity.Text);
+            }
+            if (SearchAllCheckBox.Checked == false) //eğer tek bir bina seçildiyse
+            {
+                DropDownList building = (DropDownList)LocationDetailsView.Rows[0].FindControl("buildingDropDownList");
+                sorgu = sorgu + " AND locationBuildingId ='" + building.SelectedValue + "'";
+            }
+            Session["LocationFilter"] = sorgu; //filter bilgisini başka eventlerde kullanmak için session'da bir değişken olarak saklarız
+            Location_SqlDataSource.FilterExpression = sorgu;
+        }
+
+        protected void ApplyFilter()
+        {
+            //bu yordam filtrelenmiş bir satırın edit edilebilmesi ve herhangi bir işlem yapıldıktan sonra filtrenin hala kalması için yazıldı
+            Location_SqlDataSource.FilterExpression = (string)(Session["LocationFilter"]);
+            LocationGridView.DataBind();
+        }
+
+        protected void LocationGridView_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            ApplyFilter();
+        }
+
+        protected void LocationGridView_RowUpdated(object sender, GridViewUpdatedEventArgs e)
+        {
+            ApplyFilter();
+        }
+
     }
 }
