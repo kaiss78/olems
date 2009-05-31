b17d0d689c6d9d2956d76933752a2e9534b41b15
erdem.hale <erdem.hale@7d986af4-2e36-11de-ae9b-2be1a451ffb1>
2009-05-31 17:18:16 +0000

    Topic Management arayuzu bitti, ornek olarak kullanilabilir Hale

---
+++ trunk/code/OLEMS/OLEMS/QuestionDevelopment/TopicManagement.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void lnkSearch_Click(object sender, EventArgs e)
+    {
+        String sorgu = "";
+        TextBox txtTopic = (TextBox)TopicDetailsView.Rows[0].FindControl("txtName");
+        sorgu = "name like '%" + txtTopic.Text + "%'";
+        Session["TopicFilter"] = sorgu;//filter bilgisini başka eventlerde kullanmak için session'da bir değişken olarak saklarız
+        Topic_SqlDataSource.FilterExpression = sorgu;
+    }
+    
+    protected void ApplyFilter()
+    {
+        //bu yordam filtrelenmiş bir satırın edit edilebilmesi ve herhangi bir işlem yapıldıktan sonra filtrenin hala kalması için yazıldı
+        Topic_SqlDataSource.FilterExpression = (string)(Session["TopicFilter"]);
+        TopicGridView.DataBind();
+    }
+    
+    protected void TopicGridView_RowEditing(object sender, GridViewEditEventArgs e)
+    {
+        ApplyFilter();    
+    }
+    
+    protected void TopicGridView_RowUpdated(object sender, GridViewUpdatedEventArgs e)
+    {
+        ApplyFilter();  
+    }
+}
