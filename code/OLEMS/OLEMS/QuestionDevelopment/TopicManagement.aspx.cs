706e0e0866a56f2c2cb698c6ce7440afaee0231a
erdem.hale <erdem.hale@7d986af4-2e36-11de-ae9b-2be1a451ffb1>
2009-05-31 17:56:33 +0000

    duzenlemeler

--- code/OLEMS/OLEMS/QuestionDevelopment/TopicManagement.aspx.cs
+++ code/OLEMS/OLEMS/QuestionDevelopment/TopicManagement.aspx.cs
@@ -11,36 +11,38 @@ using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
 using System.Xml.Linq;
 
-public partial class _Default : System.Web.UI.Page
+namespace OLEMS.QuestionDevelopment
 {
-    protected void Page_Load(object sender, EventArgs e)
+    public partial class TopicManagement : System.Web.UI.Page
     {
+        protected void Page_Load(object sender, EventArgs e)
+        {
 
-    }
+        }
+        protected void lnkSearch_Click(object sender, EventArgs e)
+        {
+            String sorgu = "";
+            TextBox txtTopic = (TextBox)TopicDetailsView.Rows[0].FindControl("txtName");
+            sorgu = "name like '%" + txtTopic.Text + "%'";
+            Session["TopicFilter"] = sorgu;//filter bilgisini başka eventlerde kullanmak için session'da bir değişken olarak saklarız
+            Topic_SqlDataSource.FilterExpression = sorgu;
+        }
 
-    protected void lnkSearch_Click(object sender, EventArgs e)
-    {
-        String sorgu = "";
-        TextBox txtTopic = (TextBox)TopicDetailsView.Rows[0].FindControl("txtName");
-        sorgu = "name like '%" + txtTopic.Text + "%'";
-        Session["TopicFilter"] = sorgu;//filter bilgisini başka eventlerde kullanmak için session'da bir değişken olarak saklarız
-        Topic_SqlDataSource.FilterExpression = sorgu;
-    }
-    
-    protected void ApplyFilter()
-    {
-        //bu yordam filtrelenmiş bir satırın edit edilebilmesi ve herhangi bir işlem yapıldıktan sonra filtrenin hala kalması için yazıldı
-        Topic_SqlDataSource.FilterExpression = (string)(Session["TopicFilter"]);
-        TopicGridView.DataBind();
-    }
-    
-    protected void TopicGridView_RowEditing(object sender, GridViewEditEventArgs e)
-    {
-        ApplyFilter();    
-    }
-    
-    protected void TopicGridView_RowUpdated(object sender, GridViewUpdatedEventArgs e)
-    {
-        ApplyFilter();  
+        protected void ApplyFilter()
+        {
+            //bu yordam filtrelenmiş bir satırın edit edilebilmesi ve herhangi bir işlem yapıldıktan sonra filtrenin hala kalması için yazıldı
+            Topic_SqlDataSource.FilterExpression = (string)(Session["TopicFilter"]);
+            TopicGridView.DataBind();
+        }
+
+        protected void TopicGridView_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            ApplyFilter();
+        }
+
+        protected void TopicGridView_RowUpdated(object sender, GridViewUpdatedEventArgs e)
+        {
+            ApplyFilter();
+        }
     }
 }
