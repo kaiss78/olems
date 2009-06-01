ded41ae95cd875be0fa88e91c0576b92bffdbd4a
erdem.hale <erdem.hale@7d986af4-2e36-11de-ae9b-2be1a451ffb1>
2009-06-01 19:23:52 +0000

    messagebox ve masterpage eklendi

--- code/OLEMS/OLEMS/QuestionDevelopment/TopicManagement.aspx.designer.cs
+++ code/OLEMS/OLEMS/QuestionDevelopment/TopicManagement.aspx.designer.cs
@@ -14,24 +14,6 @@ namespace OLEMS.QuestionDevelopment {
     public partial class TopicManagement {
         
         /// <summary>
-        /// Head1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlHead Head1;
-        
-        /// <summary>
-        /// form1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-        
-        /// <summary>
         /// baslik control.
         /// </summary>
         /// <remarks>
